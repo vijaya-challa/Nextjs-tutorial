5596853b43952b7e97753a91137be1c5662913ac
vijaya-challa <vijayajay1814@gmail.com>
2024-01-12 14:43:29 +0100

    added home page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const aboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className={styles.imgContainer}>
+      <Image
+        src="https://cdn.pixabay.com/photo/2023/11/02/16/49/cat-8361048_1280.jpg"
+        alt=""
+        fill
+      />
+    </div>
+  );
 };
 
 export default aboutPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import styles from "./home.module.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello there</h1>
-    </main>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab rem quia
+          placeat perferendis? Officia, dignissimos.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
   );
 }
